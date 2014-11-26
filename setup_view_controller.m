59d7540e4a553980e1cbfaa56b7d4749a4feea9f
tannauit <tannguyenanh@gmail.com>
2014-11-26 11:41:31 +0700

    [f] add setup view controller

---
+++ setup_view_controller.m
@@ -0,0 +1,28 @@
+// SetupViewController
+// SetupViewController
+//
+// IDECodeSnippetCompletionPrefix: setup-View-Controller
+// IDECodeSnippetCompletionScopes: [ClassImplementation]
+// IDECodeSnippetIdentifier: 2ff4cad0-7526-11e4-b116-123b93f75cba
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self initUI];
+    [self prepareUI];
+}
+
+#pragma mark -- Prepare Stuffs
+- (void)prepareUI {
+
+}
+
+#pragma mark -- Decorate Stuffs
+- (void)initUI {
+
+}
+
+#pragma mark -- User Interacted
+
