dfe1d3521445cd5424a7e793b86989af49e74617
tannauit <tannguyenanh@gmail.com>
2014-12-09 09:52:12 +0700

    [f] swift mark

---
+++ swift_mark.m
@@ -0,0 +1,17 @@
+// swift_mark
+// swift_mark
+//
+// IDECodeSnippetCompletionPrefix: swift_mark
+// IDECodeSnippetCompletionScopes: [ClassImplementation]
+// IDECodeSnippetIdentifier: 45c13f7e-7f4e-11e4-b116-123b93f75cba
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Swift
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+
+    // MARK: EVENT
+
+    // MARK: UI
+
+    // MARK: MISC
+
+    //MARK: Others
